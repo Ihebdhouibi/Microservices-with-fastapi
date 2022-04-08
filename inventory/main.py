4136306adc98669418ba095b2606b2cb6657d516
iheb dhouibi <ihebdhouibi@polytechnicien.tn>
2022-04-08 01:50:04 +0100

    Product class done

--- inventory/main.py
+++ inventory/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from redis_om import get_redis_connection
+from redis_om import get_redis_connection, HashModel
 
 redis = get_redis_connection(
     host="redis-11349.c135.eu-central-1-1.ec2.cloud.redislabs.com",
@@ -7,3 +7,15 @@ redis = get_redis_connection(
     password="fvCn9Ug0pkAv480mBMlf1kd8oa90JpJ1",
     decode_responses=True,
 )
+
+
+# This class will generate models that will be converted to redis tables.
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
+
+    class Meta:
+        database = redis
+
+

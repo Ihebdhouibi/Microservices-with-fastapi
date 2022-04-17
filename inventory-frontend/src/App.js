d68a3b767abe695a5bf8d978e2581966b433d099
iheb dhouibi <ihebdhouibi@polytechnicien.tn>
2022-04-17 01:00:10 +0100

    inventory-frontend/ css added

--- inventory-frontend/src/App.js
+++ inventory-frontend/src/App.js
@@ -6,16 +6,16 @@ function App() {
   return (
     <div className="App">
       <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Inventory</a>
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-     
-          <div class="navbar-nav">
-            <div class="nav-item text-nowrap">
-              <a class="nav-link px-3" href="#">Sign out</a>
-            </div>
+
+        <div class="navbar-nav">
+          <div class="nav-item text-nowrap">
+            <a class="nav-link px-3" href="#">Sign out</a>
           </div>
+        </div>
       </header>
 
       <div class="container-fluid">
@@ -23,12 +23,6 @@ function App() {
           <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
             <div class="position-sticky pt-3">
               <ul class="nav flex-column">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">
-                    <span data-feather="home"></span>
-                    Dashboard
-                  </a>
-                </li>
                 <li class="nav-item">
                   <a class="nav-link" href="#">
                     <span data-feather="file"></span>
@@ -41,19 +35,14 @@ function App() {
                     Products
                   </a>
                 </li>
-               
+
               </ul>
 
-    
+
             </div>
           </nav>
 
           <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-           
-
-            <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
-
-            <h2>Section title</h2>
             <div class="table-responsive">
               <table class="table table-striped table-sm">
                 <thead>
@@ -66,7 +55,7 @@ function App() {
                   </tr>
                 </thead>
                 <tbody>
-                  
+
                 </tbody>
               </table>
             </div>

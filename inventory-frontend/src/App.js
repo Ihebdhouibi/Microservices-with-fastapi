b6fb841e1a7e4f2f78482ad6453164fc26e109d7
iheb dhouibi <ihebdhouibi@polytechnicien.tn>
2022-04-17 01:38:24 +0100

    inventory-frontend/ Products component created

--- inventory-frontend/src/App.js
+++ inventory-frontend/src/App.js
@@ -1,68 +1,11 @@
+import { Products } from './components/Products';
 import logo from './logo.svg';
 
 
 
 function App() {
   return (
-    <div className="App">
-      <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Inventory</a>
-        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="navbar-nav">
-          <div class="nav-item text-nowrap">
-            <a class="nav-link px-3" href="#">Sign out</a>
-          </div>
-        </div>
-      </header>
-
-      <div class="container-fluid">
-        <div class="row">
-          <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-            <div class="position-sticky pt-3">
-              <ul class="nav flex-column">
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
-                    <span data-feather="file"></span>
-                    Orders
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
-                    <span data-feather="shopping-cart"></span>
-                    Products
-                  </a>
-                </li>
-
-              </ul>
-
-
-            </div>
-          </nav>
-
-          <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div class="table-responsive">
-              <table class="table table-striped table-sm">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                  </tr>
-                </thead>
-                <tbody>
-
-                </tbody>
-              </table>
-            </div>
-          </main>
-        </div>
-      </div>
-    </div>
+    <Products />
   );
 }
 
---
+++ inventory-frontend/src/components/Products.js
@@ -0,0 +1,22 @@
+import { Wrapper } from "./Wrapper"
+
+export const Products = () => {
+    return <Wrapper>
+        <div class="table-responsive">
+            <table class="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Header</th>
+                        <th scope="col">Header</th>
+                        <th scope="col">Header</th>
+                        <th scope="col">Header</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+    </Wrapper>
+}

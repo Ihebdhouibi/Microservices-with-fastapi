af894913b5dcc7aca255107f7fa712a562889a30
iheb dhouibi <ihebdhouibi@polytechnicien.tn>
2022-04-20 06:06:21 +0100

    inventory-frontend/ Products route added

--- inventory-frontend/src/App.js
+++ inventory-frontend/src/App.js
@@ -1,12 +1,15 @@
 import { Products } from './components/Products';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 
 
 
 function App() {
-  return (
-    <Products />
-  );
+  return <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Products />} />
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App;

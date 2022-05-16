930ab3cd30e526cf827c63a27efe9ceb1e90842e
iheb dhouibi <ihebdhouibi@polytechnicien.tn>
2022-05-16 14:57:31 +0100

    inventory/ Creating product yield 422 unprocessable entity

--- inventory/main.py
+++ inventory/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
+from pydantic import BaseModel
 
 app = FastAPI()
 
